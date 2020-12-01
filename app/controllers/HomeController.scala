abdc356d883e7725dabcb1bd1c66c80ce9fb051a
Tanner Smith <tanner.smith1125@gmail.com>
2020-12-01 17:05:53 -0500

    All links now interact as anticipated when clicked.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -22,20 +22,31 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.pokedex1("Test"))
+    Ok(views.html.pokedex1("A Scala Pokedex"))
   }
 
   def pokedex3(name: String) = Action { implicit request: Request[AnyContent] =>
     var poke = rest.getPokemon(name)
-    Ok(views.html.pokedex3("Test")(poke))
+    Ok(views.html.pokedex3("A Scala Pokedex")(poke))
   }
 
   def battle1() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.battle1())
   }
 
-  def battle3() = Action { implicit request: Request[AnyContent] =>
-    var winner = rest.getPokemon("Mewtwo")
-    Ok(views.html.battle3(winner))
+  def battle3(name1:String, name2: String) = Action { implicit request: Request[AnyContent] =>
+    var p1 = rest.getPokemon(name1)
+    var p2 = rest.getPokemon(name2)
+
+    val battle = manager.createBattle(p1, p2)
+
+    Ok(views.html.battle3(battle.winner))
+  }
+
+  def allBattles() = Action { implicit request: Request[AnyContent] =>
+    var result = "Previous Battles:\n"
+    manager.allPreviousBattles.foreach(battle => result += battle.toString + "\n")
+
+    Ok(result)
   }
 }
--- app/models/Battle.scala
+++ app/models/Battle.scala
@@ -2,10 +2,16 @@ package models
 
 class Battle(var pkmn1: Pokemon, var pkmn2: Pokemon) {
   var winner: Pokemon = null;
+  var loser: Pokemon = null;
 
   //used for simple comparison of battles.
   val combatants: Set[String] = Set(pkmn1.name, pkmn2.name)
 
   var completed: Boolean = false
+
+  //
+  override def toString: String = {
+    winner.name + " defeats " + loser.name
+  }
 
 }
